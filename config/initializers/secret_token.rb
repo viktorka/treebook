f8c1777b6d9a7e3f69ec76e7d05253650b1834e6
Viktorka <viktorka.mail@gmail.com>
2012-10-26 22:31:06 +0200

    Just created treebok app and ...

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = '334e06eb42fc7e8b9a011e4b233303fa0bdab756453ab9ef4a4ef65ff7d84ebebff6834dd2c5e43dced331a5d2148d9a3b77b35fd516a7e3b59ccb63ed3e5ced'
